7883da9c006caa29030eea7f59128d923bc7ffae
johnsmm <admin@ofps.net>
2016-07-21 23:27:43 -0700

    Disabling VON for Global

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -17,6 +17,8 @@ onLoadName = "OFPS Benny Warfare";
 
 briefingName = "OFPS Benny Warfare";
 
+0 enableChannel [true, false]; // Enable user ability to send text but disable voice on global channel
+
 //--- ArmA 3 Specifics
 dev = "Benny";
 author = "Benny";
