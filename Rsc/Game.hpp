65c0d79240fa2d73bc6d168d56b87bc6ac7acdaa
Liveandletdie <leifzim@live.com>
2016-09-24 13:57:58 -0700

    disable revive

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -7,15 +7,15 @@ respawnDialog = false;
 //respawnTemplates[] = {"Revive"};
 //respawnOnStart = -1;
 //-- Respawn Revive
-ReviveMode = 1;                         //0: disabled, 1: enabled, 2: controlled by player attributes
-ReviveUnconsciousStateMode = 1;         //0: basic, 1: advanced, 2: realistic
-ReviveRequiredTrait = 0;                //0: none, 1: medic trait is required
-ReviveRequiredItems = 0;                //0: none, 1: medkit, 2: medkit or first aid kit
-ReviveRequiredItemsFakConsumed = 0;     //0: first aid kit is not consumed upon revive, 1: first aid kit is consumed
-ReviveDelay = 6;                        //time needed to revive someone (in secs)
-ReviveMedicSpeedMultiplier = 2;         //speed multiplier for revive performed by medic
-ReviveForceRespawnDelay = 3;            //time needed to perform force respawn (in secs)
-ReviveBleedOutDelay = 120;              //unconscious state duration (in secs)
+//ReviveMode = 1;                         //0: disabled, 1: enabled, 2: controlled by player attributes
+//ReviveUnconsciousStateMode = 1;         //0: basic, 1: advanced, 2: realistic
+//ReviveRequiredTrait = 0;                //0: none, 1: medic trait is required
+//ReviveRequiredItems = 0;                //0: none, 1: medkit, 2: medkit or first aid kit
+//ReviveRequiredItemsFakConsumed = 0;     //0: first aid kit is not consumed upon revive, 1: first aid kit is consumed
+//ReviveDelay = 6;                        //time needed to revive someone (in secs)
+//ReviveMedicSpeedMultiplier = 2;         //speed multiplier for revive performed by medic
+//ReviveForceRespawnDelay = 3;            //time needed to perform force respawn (in secs)
+//ReviveBleedOutDelay = 120;              //unconscious state duration (in secs)
 
 //--- Require briefing.html to show up.
 onLoadMission = "OFPS Benny Warfare";
